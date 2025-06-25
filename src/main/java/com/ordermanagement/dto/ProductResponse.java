d58a68fe31b06e2e6567e8c30318d49884f7c1cf
layakishore7 <layakishore4947@gmail.com>
2025-06-25 22:20:33 +0530

    implemented add category api

---
+++ src/main/java/com/ordermanagement/dto/ProductResponse.java
@@ -0,0 +1,71 @@
+package com.ordermanagement.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+
+    private String name;
+
+    private String sku;
+
+    private Integer categoryId;
+
+    private Integer price;
+
+    private String description;
+
+    private Integer stock;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/com/ordermanagement/entity/Product.java
+++ src/main/java/com/ordermanagement/entity/Product.java
@@ -4,6 +4,7 @@ package com.ordermanagement.entity;
 import com.ordermanagement.Enum.Enum;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -28,6 +29,8 @@ public class Product {
 
     private Integer quantity;
 
+    private String description;
+
     @Enumerated(EnumType.STRING)
     private Enum.ProductStatus status;
 
@@ -38,16 +41,81 @@ public class Product {
     @JoinColumn(name = "category_id",nullable = false)
     private Category category;
 
-    @OneToMany(mappedBy = "product")
-    private List<OrderItem> orderItems;
-
-    @OneToMany(mappedBy = "product")
-    private List<Inventory> inventory;
-
-
 
     @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Enum.ProductStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(Enum.ProductStatus status) {
+        this.status = status;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
